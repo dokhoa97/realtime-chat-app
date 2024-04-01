bce23a3d44f8d9613f95109754ab99b76a874ad3
dokhoa97 <waiting.tone97@gmail.com>
2024-04-01 15:07:47 +0700

    build server completely

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require('mongoose');
+const userRouter = require('./Routes/userRoute');
+
+
+const app = express();
+require('dotenv').config();
+
+app.use(express.json());
+app.use(cors());
+app.use('/api/users', userRouter)
+app.get('/', (req, res) => {
+    res.send('Welcome our chat app APIs..')
+});
+
+const port = process.env.PORT || 3030;
+const uri = process.env.ATLAS_URI;
+
+app.listen(port, (req, res) => {
+    console.log(`Server running on port: ${port}`)
+});
+
+mongoose
+    .connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => console.log('MongoDB connection established'))
+    .catch((error) => console.log('MongoDB connection failed: ', error.message));
