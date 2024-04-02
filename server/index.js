5d49fb5d6c4fd3dcaed465b3ac5af7f52ead2ebc
dokhoa97 <waiting.tone97@gmail.com>
2024-04-02 14:10:14 +0700

    server-chatmodel-messagemodel

--- server/index.js
+++ server/index.js
@@ -2,14 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require('mongoose');
 const userRouter = require('./Routes/userRoute');
-
+const chatRouter = require('./Routes/chatRoute');
+const messageRouter = require('./Routes/messageRoute');
 
 const app = express();
 require('dotenv').config();
 
 app.use(express.json());
 app.use(cors());
-app.use('/api/users', userRouter)
+app.use('/api/messages', messageRouter);
+app.use('/api/users', userRouter);
+app.use('/api/chats', chatRouter);
+
+
 app.get('/', (req, res) => {
     res.send('Welcome our chat app APIs..')
 });
